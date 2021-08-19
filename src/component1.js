8a7401b8cddd9faf5d8fe61323bc53ebfa8fea9a
hyokyun <gyrbs22@gmail.com>
2021-08-19 20:14:08 +0900

    set component, itself

---
+++ src/component1.js
@@ -0,0 +1,9 @@
+import react from 'react'
+
+function foo() {
+    return(
+        <h3>This is the first component ever!</h3>
+    )
+}
+
+export default foo
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import foo from './component1'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
