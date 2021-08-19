5535d7a4b229cf556858d0e899d22719980c901d
hyokyun <gyrbs22@gmail.com>
2021-08-20 00:13:55 +0900

    map/props logic modified

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import './App.css';
 
 const foodList = [
   {
+    id : 1,
     name : "Ramen",
     rating : 9
   },
   {
+    id : 2,
     name : "soup",
     rating : 7
   },
   {
+    id : 3,
     name : "rice",
     rating : 8
   }
 ]
 
-function List({props}) {
+function List({name, rating}) {
   return (
     <>
   <h2>This is children component!</h2>
-  <h3>My favorite food's name is {props}</h3>
+  <h3>My favorite food's name is {name}</h3>
     </>
   )
 }
@@ -30,7 +33,7 @@ function App() {
   return (
     <div>
       <h1>Hello</h1>
-      {foodList.map(item => (<List name={item.name}/>))}
+      {foodList.map( item => (<List key={item.id} name={item.name} rating={item.rating}/>))}
     </div>
   );
 }
